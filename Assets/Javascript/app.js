a86e11801e4a7be5085e06c5dccf67317254aba9
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-05-05 16:21:42 -0700

    Cleaned up the javascript file

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -5,22 +5,9 @@ $(document).ready(function(){
     let wrongAnswers = 0;
     let answered = 0;
     let unAnswered = 0;
-    let timer = ;
     let answers = ["yes", "no","maybe So"];
 
-    //.. Our functions
-    let start = function{
-        $("#strtBtn").on("click", function){
 
-        }
-
-    }
-
-    let reset = function{
-        correctAnswers = 0;
-        timer = ;
-
-    }
     //..
 
 
