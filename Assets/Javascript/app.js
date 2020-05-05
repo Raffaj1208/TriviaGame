6d0713ce88b0677ef7ec65cbd7098febe476c34a
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-05-05 14:59:07 -0700

    mapping everything out

---
+++ Assets/Javascript/app.js
@@ -0,0 +1,27 @@
+$(document).ready(){
+
+    //.. Our variables
+    let correctAnswers = 0;
+    let wrongAnswers = 0;
+    let answered = 0;
+    let timer = ;
+    let answers = [];
+
+    //.. Our functions
+    let start = function{
+        $("#strtBtn").on("click", function){
+
+        }
+
+    }
+
+    let reset = function{
+        correctAnswers = 0;
+        timer = ;
+        
+    }
+    //..
+
+
+
+}
