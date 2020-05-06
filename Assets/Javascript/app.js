7247dbc9d24dab48ecada6bf799125066125191e
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-05-06 13:44:20 -0700

    creating timer layout

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,13 +1,35 @@
-$(document).ready(function(){
-
     //.. Our variables
     let correctAnswers = 0;
     let wrongAnswers = 0;
     let answered = 0;
     let unAnswered = 0;
-    let answerOne = ["yes", "no","maybe So"];
+    let questionOne = ["yes", "no","maybe So"];
+    let answerOne = "no";
+    let counter = 0;
+    let timeOut = 65;
+
     //..
-    $("")
 
+    function setup(){
+        let timer = select("#timer");
+        $("#timer").html(timeOut - counter);
+    }
 
-}
+    let interval = setInterval(timeIt, 1000);
+
+    function timeIt() {
+        counter++;
+        $("#timer").html(timeOut - counter);
+        if (timeOut === counter){
+            clearInterval(interval);
+            
+
+        }
+    }
+    
+    //..
+ 
+    
+    //..
+   
+
