fe9d520c8e6b3575fb612355f44dace192e4bfb9
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-05-06 15:32:08 -0700

    added potential theme in css

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -3,7 +3,7 @@
     let wrongAnswers = 0;
     let answered = 0;
     let unAnswered = 0;
-    let answersOne = ["yes", "no","maybe So"];
+    let answersOne = ["5", "3","4"];
     let answersTwo = ["yes"];
     let answersThree = ["no"];
     let answersFour = ["maybe"];
@@ -14,24 +14,41 @@
     let counter = 0;
     let timeOut = 65;
     //..
-    
+    //..This is for block one
     $("#question1").html(questionOne);
-    $(".one").html(answersOne);
+    $("#oneOne").html(answersOne[0]);
+    $("#oneTwo").html(answersOne[1]);
+    $("#oneThree").html(answersOne[2]);
+    //.. Block 2
     $("#question2").html(questionTwo);
     $(".two").html(answersTwo);
+    //.. Block 3
     $("#question3").html(questionThree);
     $(".three").html(answersThree);
+    //.. Block 4
     $("#question4").html(questionFour);
     $(".four").html(answersFour);
-    //.
-    for (i = 0; i < answersOne.length; i++){
-        let multipleChoiceOne = answersOne[i]; 
-    }
 
     //.. 
-    function setup(){
-        let timer = select("#timer");
-        $("#timer").html(timeOut - counter);
+    function start(){
+        $("#strtBtn").on("click", function() {
+            setup();
+            timeIt();
+        });
+        //..This is for block one
+    $("#question1").html(questionOne);
+    $("#oneOne").html(answersOne[0]);
+    $("#oneTwo").html(answersOne[1]);
+    $("#oneThree").html(answersOne[2]);
+    //.. Block 2
+    $("#question2").html(questionTwo);
+    $(".two").html(answersTwo);
+    //.. Block 3
+    $("#question3").html(questionThree);
+    $(".three").html(answersThree);
+    //.. Block 4
+    $("#question4").html(questionFour);
+    $(".four").html(answersFour);
     }
 
     let interval = setInterval(timeIt, 1000);
@@ -41,7 +58,5 @@
         $("#timer").html(timeOut - counter);
         if (timeOut === counter){
             clearInterval(interval);
-            
-
         }
     }
