b53f5dd9beb58f27b376fe5a019956677105aa26
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-05-06 14:27:31 -0700

    changed location of text from inside the html file to the javascript file to be able to manipulate it

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -3,13 +3,32 @@
     let wrongAnswers = 0;
     let answered = 0;
     let unAnswered = 0;
-    let questionOne = ["yes", "no","maybe So"];
-    let answerOne = "no";
+    let answersOne = ["yes", "no","maybe So"];
+    let answersTwo = ["yes"];
+    let answersThree = ["no"];
+    let answersFour = ["maybe"];
+    let questionOne = "This is a question";
+    let questionTwo = "This is another question";
+    let questionThree = "This is the 3rd question";
+    let questionFour = " This is the 4th and final question";
     let counter = 0;
     let timeOut = 65;
-
     //..
+    
+    $("#question1").html(questionOne);
+    $(".one").html(answersOne);
+    $("#question2").html(questionTwo);
+    $(".two").html(answersTwo);
+    $("#question3").html(questionThree);
+    $(".three").html(answersThree);
+    $("#question4").html(questionFour);
+    $(".four").html(answersFour);
+    //.
+    for (i = 0; i < answersOne.length; i++){
+        let multipleChoiceOne = answersOne[i]; 
+    }
 
+    //.. 
     function setup(){
         let timer = select("#timer");
         $("#timer").html(timeOut - counter);
@@ -26,10 +45,3 @@
 
         }
     }
-    
-    //..
- 
-    
-    //..
-   
-
