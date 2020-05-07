f8a726f6a2319811884409d2a8237e81231d7c90
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-05-07 15:44:25 -0700

    finished algorithm

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -20,18 +20,79 @@
     //..
     function checked(){
         let a1 = document.getElementById("a1").checked;
-        let b1 = document.getElementById("a2").checked;
-        let c1 = document.getElementById("a3").checked;
+        let b1 = document.getElementById("b1").checked;
+        let c1 = document.getElementById("c1").checked;
+        let a2 = document.getElementById("a2").checked;
+        let b2 = document.getElementById("b2").checked;
+        let c2 = document.getElementById("c2").checked;
+        let a3 = document.getElementById("a3").checked;
+        let b3 = document.getElementById("b3").checked;
+        let c3 = document.getElementById("c3").checked;
+        let a4 = document.getElementById("a4").checked;
+        let b4 = document.getElementById("b4").checked;
+        let c4 = document.getElementById("c4").checked;
+
+        // Question 1
         if (a1 == true && b1 == false && c1 == false){
             correctAnswers++;
+            answered++;
+            results();
+        } else if (a1 == false){
+            wrongAnswers++;
+            answered++;
+            results();
+        } else if (a1 == false && b1 == false && c1 == false){
+            unAnswered++;
+            wrongAnswers++;
+            results();
+        }
+
+        // Question 2
+        if (a2 == false && b2 == false && c2 ==true){
+            answered++;
+            correctAnswers++;
+            results();
+        } else if (c2 == false){
+            wrongAnswers++;
+            answered++;
+            results();
+        } else if (a2 == false && b2 == false && c2 == false){
+            wrongAnswers++;
+            unAnswered++;
+            results();
+        }
+
+        // Question 3
+        if (a3 == false && b3 == true && c3 == false){
+            answered++;
+            correctAnswers++;
+            results();
+        } else if ( b3 == false){
+            answered++;
+            wrongAnswers++;
+            results();
+        } else if (a3 == false && b3 == false && c3 == false){
+            unAnswered++;
+            wrongAnswers++;
+            results();
+        }
+        
+        // Question 4
+        if (a4 == true && b4 == false && c4 == false){
+            correctAnswers++;
+            answered++;
+            results();
+        } else if (a4 == false){
+            wrongAnswers++;
+            answered++;
+            results();
+        } else if (a4 == false && b4 == false && c4 == false){
+            wrongAnswers++;
+            unAnswered++;
             results();
         }
     }
 
-    // 
-    function unchecked(){
-
-    }
     //..
     function results() {
         $("main").hide();
@@ -47,7 +108,7 @@
     function timeIt() {
             $("#timer").html(timeOut - counter);
             if (timeOut === counter){
-                results();
+                checked();
                 clearInterval(interval)
             } else {
                 counter++;
